f8a85dcf2eb3b1c856b296331240bbbc64d807bb
Elkira01 <gilles.mbissileu@facsciences-uy1.cm>
2024-07-26 06:43:52 +0100

    ddate input

--- src/app/(admin)/dashboard/login/page.tsx
+++ src/app/(admin)/dashboard/login/page.tsx
@@ -1,14 +1,14 @@
 'use client'
 
 import {AdminLoginForm} from '@/app/(admin)/_components/forms';
-import {CardContainer} from "@/app/(admin)/_components/container/CardContainer";
+import {Card} from "@/components/ui/card";
 export default function AdminLogin(){
 
     return(
         <div className='w-full border-2 border-dashed py-10 border-black'>
-            <CardContainer $width='50%'>
+            <Card className={`w-1/2 h-fit flex flex-row gap-[1rem] p-[1.25rem]`}>
                 <AdminLoginForm/>
-            </CardContainer>
+            </Card>
         </div>
     )
 }
--- src/app/shared/components/form/inputs/DateInput/index.tsx
+++ src/app/shared/components/form/inputs/DateInput/index.tsx
@@ -2,7 +2,6 @@ import { format } from "date-fns"
 import { CalendarIcon } from "lucide-react"
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
-// import { Calendar } from "@/components/ui/calendar"
 import {
     FormControl,
     FormDescription,
@@ -11,7 +10,10 @@ import {
     FormLabel,
     FormMessage,
 } from "@/components/ui/form"
-export interface DateInputProps {
+import {Popover, PopoverContent, PopoverTrigger} from "@/components/ui/popover";
+import {Calendar} from "@/components/ui/calendar";
+import {InputProps} from "react-day-picker";
+export interface DateInputProps extends InputProps {
     formControl: any,
     name: any,
     label: any,
@@ -26,31 +28,36 @@ export default function DatePicker({ formControl, name, label, description, ...p
             render={({ field }) => (
                 <FormItem className="flex flex-col">
                     <FormLabel>{label ?? ''}</FormLabel>
-                        <FormControl>
-                            <Button
-                                variant={"outline"}
-                                className={cn(
-                                    "w-full pl-3 text-left font-normal",
-                                    !field.value && "text-muted-foreground"
-                                )}
-                            >
-                                {field.value ? (
-                                    format(field.value, "PPP")
-                                ) : (
-                                    <span>Pick a date</span>
-                                )}
-                                <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                            </Button>
-                        </FormControl>
-                        {/*<Calendar*/}
-                        {/*    mode="single"*/}
-                        {/*    selected={field.value}*/}
-                        {/*    onSelect={field.onChange}*/}
-                        {/*    disabled={(date) =>*/}
-                        {/*        date > new Date() || date < new Date("1900-01-01")*/}
-                        {/*    }*/}
-                        {/*    initialFocus*/}
-                        {/*/>*/}
+                    <Popover>
+                        <PopoverTrigger asChild>
+                            <FormControl>
+                                <Button
+                                    variant={"outline"}
+                                    className={cn(
+                                        "w-full pl-3 text-left font-normal",
+                                        !field.value && "text-muted-foreground"
+                                    )}
+                                >
+                                    {field.value ? (
+                                        format(field.value, "PPP")
+                                    ) : (
+                                        <span>{props.placeholder}</span>
+                                    )}
+                                    <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                                </Button>
+                            </FormControl>
+                        </PopoverTrigger>
+                        <PopoverContent align='end'>
+                            <Calendar
+                                selected={field.value}
+                                onSelect={field.onChange}
+                                // disabled={(date) =>
+                                //     date > new Date() || date < new Date("1900-01-01")
+                                // }
+                                initialFocus
+                            />
+                        </PopoverContent>
+                    </Popover>
                     <FormDescription>
                         {description}
                     </FormDescription>
